83242350e06ffcec2a04db1d7df7eec5fc9eb58e
pc-aide <36453201+pc-aide@users.noreply.github.com>
2020-04-22 18:12:49 -0400

    Create KRShowKeyMgr.bat

---
+++ KRShowKeyMgr.bat
@@ -0,0 +1,6 @@
+::&-----------------------------
+::& Delete Cached Credentaqils - it sotres both cert data & also user pwd
+::&-----------------------------
+
+REM
+rundll32.exe keymgr.dll,KRShowKeyMgr
