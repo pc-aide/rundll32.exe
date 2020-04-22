de84685487b587d5933886fe5441a1d64dd51ed5
pc-aide <36453201+pc-aide@users.noreply.github.com>
2020-04-22 18:29:47 -0400

    Update KRShowKeyMgr.bat

--- KRShowKeyMgr.bat
+++ KRShowKeyMgr.bat
@@ -2,8 +2,10 @@
 ::& Delete Cached Credentaqils - it sotres both cert data & also user pwd
 ::& Warning: case sensitive
 ::&
+::& https://imgur.com/0Y7be1o
 ::& credWiz.exe - Backup or Restore your stored user name & pwd
 ::& Manage your credentials: 1) Web Credentials & 2) Windows Credentials
+::&
 ::& cmdKey /list - Currently stored credentials
 ::&--------------------------------------------------------------------------
 
